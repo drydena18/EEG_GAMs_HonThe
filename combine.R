5b2f1f328607ca96f8c42d0367749e06e2a43d22
Dryden Arseneau <dryden.arseneau@ontariotechu.net>
2025-07-22 04:17:05 -0400

    Update combine.R

--- combine.R
+++ combine.R
@@ -1,3 +1,5 @@
+# Again. Files corrupted. This script combines the EID + Age file to the different connections, allowing the code to work for the future. Basically, df2 files didn't have EID or Age, which are needed for GAMs. This script gives the wPLI (df2) the EID and Age (df1).
+
 df1 <- read.csv("/Users/drydena18/Desktop/Par_Num_Age_Cluster.csv")
 df2 <- read.csv("/Users/drydena18/Library/CloudStorage/OneDrive-OntarioTechUniversity/EEG_GAMs/Prepocessed_data/wPLI/between_group/gama/ant_r_ant_l_gama.csv", header = FALSE)
 
